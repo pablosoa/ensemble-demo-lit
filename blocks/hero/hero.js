6475a4f9803da0d350179d1bf71c2a49445492dc
Amy Nguyen <amyn@ensemble.com>
2023-08-22 17:01:08 -0700

    added info section to hero block

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,6 +3,33 @@
  * @param {Element} block The footer block element
  */
 
+const INFO = [
+  { statistic: "", description: "" },
+  { statistic: "", description: "" },
+  {
+    statistic: "200+",
+    description: "EMPLOYEES",
+  },
+  { statistic: "", description: "" },
+  {
+    statistic: "4",
+    description: "GLOBAL OFFICES",
+  },
+  {
+    statistic: "100+",
+    description: "CLIENTS",
+  },
+  {
+    statistic: "1995",
+    description: "ESTABLISHED",
+  },
+  {
+    statistic: "500+",
+    description: "PROJECTS DELIVERED",
+  },
+  { statistic: "", description: "" },
+];
+
 export default async function decorate(block) {
   const h1 = block.querySelector("h1");
   h1.innerHTML = "";
@@ -55,4 +82,16 @@ export default async function decorate(block) {
 
     h1.replaceWith(animationText);
   }, 2500);
+  // create info section
+  const infoSection = document.createElement("div");
+  infoSection.className = "info-section";
+
+  infoSection.innerHTML = INFO.map((i, idx) => {
+    if (!i.statistic && !i.description) {
+      return `<div class='blank block-${idx}'></div>`;
+    } else {
+      return `<a href='https://www.ensemble.com/'><div class='block-${idx}'><p class='statistic-text'>${i.statistic}</p><p class='description'>${i.description}</p></div></a>`;
+    }
+  }).join("");
+  block.appendChild(infoSection);
 }
